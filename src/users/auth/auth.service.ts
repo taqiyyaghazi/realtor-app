53751fe0fc752be4aedb58a111ff9882ef86757e
Taqiyya Ghazi <taqiyyaghazi@gmail.com>
2023-08-19 21:54:48 +0700

    create api signup

---
+++ src/users/auth/auth.service.ts
@@ -0,0 +1,41 @@
+import { ConflictException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcryptjs';
+import { UserType } from '@prisma/client';
+
+interface iSignupParams {
+  email: string;
+  password: string;
+  name: string;
+  phone: string;
+}
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly prismaService: PrismaService) {}
+  async signup({ email, password, name, phone }: iSignupParams) {
+    const userExists = await this.prismaService.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (userExists) {
+      throw new ConflictException();
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await this.prismaService.user.create({
+      data: {
+        email,
+        name,
+        password: hashedPassword,
+        phone,
+        user_type: UserType.BUYER,
+      },
+    });
+
+    return user;
+  }
+}

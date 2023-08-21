c1792aa8be88476152cd6aa86f758edf51d5ef21
Taqiyya Ghazi <taqiyyaghazi@gmail.com>
2023-08-21 19:59:12 +0700

    add filter api get homes and create api get home

--- src/home/home.controller.ts
+++ src/home/home.controller.ts
@@ -1,18 +1,46 @@
-import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Query,
+  ParseIntPipe,
+  Param,
+} from '@nestjs/common';
 import { HomeService } from './home.service';
 import { HomeResponseDto } from './dtos/home.dto';
+import { PropertyType } from '@prisma/client';
 
 @Controller('home')
 export class HomeController {
   constructor(private readonly homeService: HomeService) {}
   @Get()
-  getHomes(): Promise<HomeResponseDto[]> {
-    return this.homeService.getHomes();
+  getHomes(
+    @Query('city') city?: string,
+    @Query('minPrice') minPrice?: string,
+    @Query('maxPrice') maxPrice?: string,
+    @Query('propertyType') propertyType?: PropertyType,
+  ): Promise<HomeResponseDto[]> {
+    const price =
+      minPrice || maxPrice
+        ? {
+            ...(minPrice && { gte: parseFloat(minPrice) }),
+            ...(maxPrice && { lte: parseFloat(maxPrice) }),
+          }
+        : undefined;
+    const filters = {
+      ...(city && { city }),
+      ...(price && { price }),
+      ...(propertyType && { property_type: propertyType }),
+    };
+
+    return this.homeService.getHomes(filters);
   }
 
   @Get(':id')
-  getHome() {
-    return {};
+  getHome(@Param('id', ParseIntPipe) id: number) {
+    return this.homeService.getHomeById(id);
   }
 
   @Post()
--- src/home/home.service.ts
+++ src/home/home.service.ts
@@ -1,11 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { HomeResponseDto } from './dtos/home.dto';
+import { PropertyType } from '@prisma/client';
+
+interface GetHomesParam {
+  city?: string;
+  price?: {
+    gte?: number;
+    lte?: number;
+  };
+  property_type?: PropertyType;
+}
 
 @Injectable()
 export class HomeService {
   constructor(private readonly prismaService: PrismaService) {}
-  async getHomes(): Promise<HomeResponseDto[]> {
+  async getHomes(filter: GetHomesParam): Promise<HomeResponseDto[]> {
+    console.log(filter);
     const homes = await this.prismaService.home.findMany({
       select: {
         id: true,
@@ -22,11 +33,29 @@ export class HomeService {
           take: 1,
         },
       },
+      where: filter,
     });
+
+    if (!homes.length) {
+      throw new NotFoundException();
+    }
+
     return homes.map((home) => {
       const fetchHome = { ...home, image: home.images[0].url };
       delete fetchHome.images;
       return new HomeResponseDto(fetchHome);
     });
+  }
+
+  async getHomeById(id: number) {
+    const home = await this.prismaService.home.findUnique({
+      where: { id },
+    });
+
+    if (!home) {
+      throw new NotFoundException();
+    }
+
+    return new HomeResponseDto(home);
   }
 }
